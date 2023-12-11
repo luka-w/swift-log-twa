13db4fce6a8efb4fb6bb61b653cda00a4d6342e6
Tomasz Wagner <61847268+luka-w@users.noreply.github.com>
2023-12-11 17:54:58 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,9 +17,6 @@ import PackageDescription
 
 let package = Package(
     name: "swift-log",
-    platforms: [
-       .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
-    ],
     products: [
         .library(name: "Logging", targets: ["Logging"]),
     ],
